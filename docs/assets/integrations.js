c7f33a6d3316065a1c81fdc1f816fa32250244df
Darien Freedman <darien.freedman@gmail.com>
2026-09-22 18:03:56 -0600

    Cleanup

--- docs/assets/integrations.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-window.cloudFirstIntegrations = Object.freeze({
-  "contactFormUrl": "https://forms.cloud.microsoft/r/LArHCapTwV?embed=true",
-  "bookingUrl": "",
-  "contactEndpoint": ""
-});
